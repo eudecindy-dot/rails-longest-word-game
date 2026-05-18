b9e7d2910c754959d17c1971316fc78c2387b22f
Cindy Eude <eudecindy@gmail.com>
2026-05-18 16:25:01 +0200

    rails new

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,15 @@
+class GamesController < ApplicationController
+
+  def new
+  @letters = params[:letter]
+  
+  end
+
+
+  def score
+  end
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
+  get "new", to: "games#new"
+  post "score", to: "games#score"
   # root "posts#index"
 end
